60b6319088f24e7bb3d87909128fc1e1e11e9c2d
Ensick <fede.censi96@gmail.com>
2022-12-13 15:12:33 +0100

    Snack-5 completato

---
+++ Snack-5/index.php
@@ -0,0 +1,10 @@
+<!-- Snack 5
+
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+
+<?php
+
+echo '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.<p> Harum ullam eum quasi accusantium facere doloribus dolores illum numquam?</p><p>Commodi rerum quo nesciunt deserunt dolores ex excepturi enim accusantium ducimus ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> Reiciendis ipsum est ex, maxime velit hic similique repellat excepturi tempora dolor sunt, nisi, ut quam voluptates officiis exercitationem unde odit quidem?</p>'
+
+?>
+
