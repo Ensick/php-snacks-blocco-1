16e3f7f0c1b8a55584815b0235aaa04213c56aef
Ensick <fede.censi96@gmail.com>
2022-12-14 10:45:31 +0100

    Fix-snack6 con correzione a lezione

--- Snack-6/index.php
+++ Snack-6/index.php
@@ -28,6 +28,8 @@ $db = [
     ]
 ];
 
+$arrayChaivi = array_keys( $db );
+
 ?>
 
 <!DOCTYPE html>
@@ -41,20 +43,68 @@ $db = [
 </head>
 <body>
 
-<?php 
 
-    for($y = 0; $y < count($db['teachers']); $y++){
 
-        echo '<div class="teachers">'.$db['teachers'][$y]['name']." ".$db['teachers'][$y]['lastname'].'</div>';
+ <!--    /* for($y = 0; $y < count($db['teachers']); $y++){
+
+        echo '<div class="teachers">';
+
+        echo $db['teachers'][$y]['name']." ".$db['teachers'][$y]['lastname'];
+
+        echo '</div>';
     }
 
     
     for($i = 0; $i < count($db['pm']); $i++ ){
 
-        echo '<div class="pm">'.$db['pm'][$i]['name']." ".$db['pm'][$i]['lastname'].'</div>';
-    };
+        echo '<div class="pm">';
+
+        echo $db['pm'][$i]['name']." ".$db['pm'][$i]['lastname'];
+
+        echo '</div>';
+    };  -->
+
    
-?>
+
+    <?php for($i = 0; $i < count( $arrayChaivi ); $i++){ ?>
+        
+        <?php if($arrayChaivi[$i] == 'teachers'){?>
+
+            <div class="teachers">
+
+            <?php for($k=0; $k < count($db['teachers']);$k++){ ?>
+
+                <ul>
+                    <li>
+                        <?php echo $db['teachers'][$k]['name']." ".$db['teachers'][$k]['lastname']?>
+                    </li>
+                </ul>
+
+            <?php } ?>
+
+            </div>
+
+        <?php } else{ ?> 
+
+            <div class="pm">
+
+            <?php for($k=0; $k < count($db['pm']);$k++){ ?>
+
+                <ul>
+                    <li>
+                        <?php echo $db['pm'][$k]['name']." ".$db['pm'][$k]['lastname']?>
+                    </li>
+                </ul>
+
+            <?php } ?>
+
+            </div>
+
+        <?php } ?>   
+
+    <?php } ?>
+   
+
    
 </body>
 </html>
